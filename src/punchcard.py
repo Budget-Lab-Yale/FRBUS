ede1a2817edd490a8039bdd2ef3ac4cb47f30a50
kendall-budget-lab <joshua.m.kendall@yale.edu>
2023-10-31 12:23:30 -0400

    ROUGH outline of import from taxsim into FRBUS

--- src/punchcard.py
+++ src/punchcard.py
@@ -22,6 +22,70 @@ def ybl_Frbus(version: str, vintage: str) -> Frbus:
     guide = os.path.join("/gpfs/gibbs/project/sarin/shared/raw_data/FRBUS", str(version), str(vintage),"model.xml")
     return Frbus(guide)
 
+def parse_tax_sim(card: DataFrame, run: int) -> DataFrame:
+    version = str(card.loc[run, "ts_version"])
+    vintage = str(card.loc[run, "ts_vintage"])
+    ts_id   = str(card.loc[run, "ID"])
+
+    ts_1040 = pandas.read_csv(os.path.join("/gpfs/gibbs/project/sarin/shared/model_data/Tax-Simulator", version, vintage, ts_id, "static/totals/1040.csv"), index_col = 0)
+
+    ts_pr = pandas.read_csv(os.path.join("/gpfs/gibbs/project/sarin/shared/model_data/Tax-Simulator", version, vintage, ts_id, "static/totals/payroll.csv"), index_col = 0)
+
+    ts = ts_1040.merge(ts_pr, how = 'left')
+    ts.index = pandas.PeriodIndex(ts.index, freq="Q")
+
+    ts.rename(columns={"liab_iit_":"gtn"})
+    ts["trp"] = ts[["liab_iit"]] / ts[["txbl_inc"]]
+    ts["trfpm"] = calc_marginal(card, run)
+    
+    return(ts[["gtn", "trp", "trfpm"]])
+
+def calc_marginal(card: DataFrame, run: int):
+
+    version = str(card.loc[run, "ts_version"])
+    vintage = str(card.loc[run, "ts_vintage"])
+    ts_id   = str(card.loc[run, "ID"])
+
+    micro_path = os.path.join("/gpfs/gibbs/project/sarin/shared/model_data/Tax-Simulator", version, vintage, ts_id, "static/detail")
+    out = numpy.array()
+
+    for filename in os.listdir(micro_path, 'files'):
+        micro = pandas.read_csv(os.path.join(micro_path, filename))
+        micro.sort_values(by=['agi'])
+        micro["csum_wgt"] = numpy.cumsum(micro["weight"]/sum(micro["weight"]))
+        temp = micro[micro.csum_wgt >= 0.5]
+        out.append(brackets(temp.head(1)))
+    
+    return(out)
+
+def brackets(fam: DataFrame):
+    # NOT USED MOST LIKELY
+
+    threshs = numpy.array(
+        [[11000, 44725, 95375,182100,231250,578125],
+        [22000,89450,190750,364200,462500,693750],
+        [11000, 44725, 95375,182100,231250,578125],
+        [15700,59850,95650,182100,231250,578100,578101]
+        ]
+    )
+    dex = fam["filing_status"]-1
+    med2 = fam["txbl_inc"] * 2
+
+    if(med2 <= threshs[dex, 0]):
+        return(10)
+    elif(med2 <= threshs[dex, 1]):
+        return(12)
+    elif(med2 <= threshs[dex, 2]):
+        return(22)
+    elif(med2 <= threshs[dex, 3]):
+        return(24)
+    elif(med2 <= threshs[dex, 4]):
+        return(32)
+    elif(med2 <= trhesh[dex, 5]):
+        return(35)
+    else:
+        return(37)
+
 def run_out(card: DataFrame, stamp: str, run: int, baseline: DataFrame, sim: DataFrame):
     # Create base path 
     path = os.path.join("/gpfs/gibbs/project/sarin/shared/model_data/FRBUS", card.loc[run, "version"], stamp, card.loc[run, "ID"])
--- src/sim_setup.py
+++ src/sim_setup.py
@@ -1,8 +1,10 @@
 import pandas
+import numpy as np
 
 from pandas import DataFrame, Period, PeriodIndex, read_csv
 from typing import Union
 from pyfrbus.load_data import load_data
+from punchcard import parse_tax_sim
 
 def levers(card: DataFrame, start: Union[str, Period], end: Union[str, Period], data: DataFrame, run: int):
     data.loc[start:end, "dfpdbt"] = card.loc[run, "dfpdbt"]
@@ -10,15 +12,51 @@ def levers(card: DataFrame, start: Union[str, Period], end: Union[str, Period], 
     data.loc[start:end, "dfpex"] = card.loc[run, "dfpex"]
     data.loc[start:end, "dmptay"] = card.loc[run, "dmptay"]
     data.loc[start:end, "dmpintay"] = card.loc[run, "dmpintay"]
-    return()
+    return(data)
 
-def sim_path(card: DataFrame, run: int, start: Union[str, Period], end: Union[str, Period], with_adds: DataFrame):
-    with_adds.loc[start, card.loc[run, "shock"]] += card.loc[run, "shockby"]
-    """
-    guide = os.path.join("/gpfs/gibbs/project/sarin/shared/model_data/FRBUS", str(card.loc[run, "version"]), str(card.loc[run, "vintage"]), "shocks.csv")
-    shocks = load_data(guide)
+def denton(I: np.array, A: np.array):
+    # This code is a modified version of the function found here:
+    # https://github.com/MaxLugo/denton
+    # It is unavailable using conda and challenging to install through the 
+    # back door. Max Lugo deserves full credit.
 
-    for col in shocks.columns:
-        with_adds.loc[start:end, col] = shocks[col]    
-    """
+    len_A, len_I, q = len(A), len(I), int(len(I)/len(A)) 
+    I_tilde = np.linalg.pinv(np.diag(I))
+    I, A = I[:, np.newaxis], A[:, np.newaxis]
+
+    D = -1 * np.eye(len_I)
+    D[-1, -1] = 0
+    for i in range(len(D)-1):
+        D[i, i+1] = 1
+
+    J = np.zeros((len_A, len_I))
+    for j in range(len(J)):
+        J[j, j*q:j*q + q] = [1]*q
+
+    M = I_tilde.T @ D.T @ D @ I_tilde
+    r1, r2 = np.concatenate((M + M.T, -J.T), axis=1), np.concatenate((J, np.zeros((len_A, len_A))), axis=1)
+    Z = np.concatenate((r1, r2), axis=0)
+    X_lambda = np.linalg.pinv(Z) @ np.concatenate((np.zeros((len_I ,1)), A), axis=0)
+    X = X_lambda[:-len_A]
+    rv = X
+    return rv
+
+def build_tax_delta(card: DataFrame, run: int, start: Union[str, Period], end: Union[str, Period], with_adds: DataFrame):
+    tax_sim_data = parse_tax_sim(card, run)
+    
+
+def sim_path(tax_sim_data: DataFrame, start: Union[str, Period], end: Union[str, Period], with_adds: DataFrame):
+        
+    with_adds.loc[start:end, "gtn_t"] = denton(
+        np.array(with_adds[start:end, "gtn"]), 
+        np.array(tax_sim_data[["gtn"]])
+    )
+    with_adds.loc[start:end, "trp_t"] = denton(
+        np.array(with_adds[start:end, "trp"]), 
+        np.array(tax_sim_data[["trp"]])
+    )
+
+
+
     return(with_adds)
+
