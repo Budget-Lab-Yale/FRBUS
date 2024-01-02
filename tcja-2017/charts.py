bbcd69c92035766cfd037c0ae18cadef09263f2b
Sarah Robinson <sarah.robinson@yale.edu>
2024-01-02 14:40:41 -0500

    Create charts with variables of interest

---
+++ tcja-2017/charts.py
@@ -0,0 +1,11 @@
+####################### FILE TO CREATE CHARTS FROM TCJA ANALYSIS #######################
+########################################################################################
+
+# Import packages #
+
+
+# Run dynamic revenue estimate program #
+exec(open('/gpfs/gibbs/project/sarin/ser68/FRBUS/tcja-2017/construct_est.py').read())
+
+
+######## Create Various graphs for analysis ########
