9c9dbe0a3a7a7d05768c9c11a6c65e34980d1196
kendall-budget-lab <joshua.m.kendall@yale.edu>
2024-02-13 13:16:15 -0500

    Clean, fast, dynamic

--- Demos/Dynamic-Revenue-Demo.py
+++ Demos/Dynamic-Revenue-Demo.py
@@ -3,7 +3,7 @@ from pandas import DataFrame, Period, PeriodIndex, read_csv
 import datetime
 import pyfrbus
 import sys
-import os
+import os 
 import numpy
 import pandas
 from numpy import array, shape, nan
@@ -23,9 +23,9 @@ frbus = Frbus("/gpfs/gibbs/project/sarin/shared/conda_pkgs/pyfrbus/models/model.
 start = pandas.Period(card.loc[0, "start"], freq="Q")
 end = pandas.Period(card.loc[0, "end"], freq="Q")
 
-baseline = build_data(card, 0, base=True, card_dates=True)
+baseline, dynamic_baseline = build_data(card, 0, base=True, card_dates=True, dynamic=True)
 with_adds = frbus.init_trac(start, end, baseline)
-dynamic_baseline = dynamic_rev(card, 0, start, end, with_adds, frbus)
+#dynamic_baseline = dynamic_rev(card, 0, start, end, with_adds, frbus)
 
 for run in range(0, len(card)):
     #### Construct and run dynamic scenario ####
@@ -33,10 +33,16 @@ for run in range(0, len(card)):
     end = pandas.Period(card.loc[run, "end"], freq="Q")
 
     path = os.path.join("/gpfs/gibbs/project/sarin/shared/model_data/FRBUS/tcja_ext",stamp, card.loc[run, "ID"])
+    
     if not os.path.exists(path):
         os.makedirs(path)
 
-    dynamic = dynamic_rev(card, run, start, end, with_adds, frbus, outpath = path)
+    if run==0:
+        dynamic = dynamic_baseline
+        baseline.to_csv(os.path.join(path, "baseline_LONGBASE.csv"))
+        #use a run out here and switch dynamic rev to be a run out
+    else:
+        dynamic = dynamic_rev(card, run, start, end, with_adds, frbus, outpath = path)
 
     #### Calculate Dynamic Revenue Delta and other Output ####
 
