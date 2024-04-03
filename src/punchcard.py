f65a5b54835161a4e33e20988072680be909219b
kendall-budget-lab <joshua.m.kendall@yale.edu>
2024-04-03 16:17:19 -0400

    cleaning for publication

--- src/punchcard.py
+++ src/punchcard.py
@@ -27,7 +27,6 @@ def parse_tax_sim(card: DataFrame, run: int, base = False) -> DataFrame:
         ID = str(card.loc[run, "ID"]) + "/conventional"
 
     ts = pandas.read_csv(os.path.join("/gpfs/gibbs/project/sarin/shared/model_data/Tax-Simulator", version, vintage, ID, "totals/receipts.csv"), index_col = 0)
-    #ts = pandas.read_csv(os.path.join("/vast/palmer/scratch/sarin/jar335/Tax-Simulator", version, vintage, ID, "totals/receipts.csv"), index_col = 0)
     ts.index = pandas.PeriodIndex(ts.index, freq="Y")
 
     ts['iit_etr'] = ts.loc[:,'revenues_income_tax'] - ts.loc[:,'outlays_tax_credits'] + ts.loc[:,'revenues_estate_tax']
@@ -38,7 +37,7 @@ def parse_corp_mtr(card: DataFrame, run: int) -> DataFrame:
     #---------------------------------------------------------------------
     # Reads in Tax Simulator's corporate output and prepares it for computation.
     # Parameters:
-    #   card   (DataFrame): Punchcard of scenario specific parameters
+    #   card   (DataFrame): Punchcard of scenario specific parameters.
     #   run          (int): Row for the card dataframe.
     # Returns:
     #   cs     (DataFrame): Tax Simulator output.
@@ -60,9 +59,9 @@ def read_macro(path: str):
     #---------------------------------------------------------------------
     # Reads in macroeconomic projections.
     # Parameters:
-    #   path           (path): Location of 
+    #   path (path): Location of projections.
     # Returns:
-    #   macro     (DataFrame): Tax Simulator output including Effective Tax Revenue
+    #   out  (DataFrame): Combined historical and projected macroeconomic data.
     #---------------------------------------------------------------------
     base = pandas.read_csv(os.path.join(path, "historical.csv"), index_col=0)
     proj = pandas.read_csv(os.path.join(path, "projections.csv"), index_col=0)
@@ -73,6 +72,16 @@ def read_macro(path: str):
     return(out)
 
 def get_housing_subsidy_rates(card: DataFrame, run: int):
+    #---------------------------------------------------------------------
+    # This function calculates the weighted average of the mortgage interest
+    #   deduction in Tax Simulator output.
+    # Parameters:
+    #   card   (DataFrame): Punchcard of scenario specific parameters.
+    #   run          (int): Row for the card dataframe.
+    # Returns:
+    #   out  (DataFrame): Baseline and scenario values for average mortgage 
+    #                       interest deducation, by year.
+    #---------------------------------------------------------------------
     start = pandas.Period(card.loc[run, "start"], freq='Q')
     end = pandas.Period(card.loc[run, "end"], freq='Q')
     outroot = os.path.join('/gpfs/gibbs/project/sarin/shared/model_data/Tax-Simulator', str(card.loc[run, "ts_version"]), str(card.loc[run, "ts_vintage"]))
@@ -97,25 +106,17 @@ def get_housing_subsidy_rates(card: DataFrame, run: int):
     return(out)
 
 def nipa_scalar(card: DataFrame, run: int):
+    #---------------------------------------------------------------------
+    # This function calculates the historical average percent of total 
+    #    government revenue made up of State and Local taxes for the 
+    #    most recent normal business cycle (2009Q2 - 2019Q4).
+    # Parameters:
+    #   card   (DataFrame): Punchcard of scenario specific parameters.
+    #   run          (int): Row for the card dataframe.
+    # Returns:
+    #   val  (dbl): average percentage.
+    #---------------------------------------------------------------------    
     tots = load_data(os.path.join("/gpfs/gibbs/project/sarin/shared/raw_data/NIPA", str(card.loc[run, "nipa_vintage"]), "3-1.csv"))
     snl = load_data(os.path.join("/gpfs/gibbs/project/sarin/shared/raw_data/NIPA", str(card.loc[run, "nipa_vintage"]), "3-3.csv"))
     return(1 + numpy.mean(snl.loc[Period("2009Q2", freq="Q"):Period("2019Q4", freq="Q"), "personal-current-taxes"] / tots.loc[Period("2009Q2", freq="Q"):Period("2019Q4", freq="Q"),"personal-current-taxes"]))
-
-def run_out(card: DataFrame, stamp: str, run: int, data: DataFrame, path = None, run_type="baseline"):
-    # Create base path 
-
-    if path is not None:
-        path = os.path.join("/gpfs/gibbs/project/sarin/shared/model_data/FRBUS", stamp)
-        
-        if not os.path.exists(path):
-            os.makedirs(path)
-
-    if run_type == "baseline":
-        data = data.filter(regex="^((?!_).)*$")
-        
-        longbase = load_data(os.path.join("/gpfs/gibbs/project/sarin/shared/raw_data/FRBUS", str(card.loc[run, "lb_version"]), str(card.loc[run, "lb_vintage"]), "LONGBASE.TXT"))
-        longbase.loc[start:end,:] = data.loc[start:end,:]
-        longbase.to_csv(os.path.join(path,card.loc[run,"ID"]+"_LONGBASE.csv"))
-    
-    return()
 
