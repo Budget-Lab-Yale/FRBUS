acfc6fdaff5da83d809b43a651f72a1998ad3f15
Josh Kendall <143127021+kendall-budget-lab@users.noreply.github.com>
2024-04-04 15:44:17 -0400

    Delete general-testing/Trajectory-Tests directory

--- general-testing/Trajectory-Tests/20231114/dsq-jobx-2023-11-14.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --output dsq-jobx-%A_%3a-%N.out
-#SBATCH --array 0-175
-#SBATCH --job-name dsq-jobx
-#SBATCH --mem-per-cpu 4g -t 60:00 --mail-type ALL
-
-# DO NOT EDIT LINE BELOW
-/vast/palmer/apps/avx2/software/dSQ/1.05/dSQBatch.py --job-file /gpfs/gibbs/project/sarin/jmk263/Repositories/FRBUS/general-testing/jobx.txt --suppress-stats-file
-
